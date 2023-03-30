e2fc9965b65889c764a014d52bd52add371e86c8
Rajan <rajand123456@gmail.com>
2023-03-30 19:47:44 -0400

    yup

--- notetake/urls.py
+++ notetake/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
     path('download/<int:file_id>/', download_file, name='download_file'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
